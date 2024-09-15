06c63d4447b497c78a0b2a9f619d4269255f6ffe
varun-liver <varun.sureshkumar@outlook.com>
2024-09-15 13:27:57 -0700

    new download js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+                    var username = 'user1';
+                    console.log('Username:', username);
+
+                    function sayHello() {
+                        alert('Hello, ' + username + ' from the dynamically created JavaScript file!');
+                    }
+                    sayHello();
+                
