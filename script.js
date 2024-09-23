3ed17cfdee80658889e6683f40c552c30bb505e3
varun-liver <varun.sureshkumar@outlook.com>
2024-09-23 13:51:46 -0700

    uhj

--- script.js
+++
@@ -1,9 +0,0 @@
-
-                    var username = 'user1';
-                    console.log('Username:', username);
-
-                    function sayHello() {
-                        alert('Hello, ' + username + ' from the dynamically created JavaScript file!');
-                    }
-                    sayHello();
-                
